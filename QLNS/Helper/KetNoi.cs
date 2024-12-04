abbdc25f061532007acabd5cf59b58e8906d8ae7
NiDa30 <145515947+NiDa30@users.noreply.github.com>
2024-12-05 03:52:48 +0700

    reset

--- QLNS/Helper/KetNoi.cs
+++ QLNS/Helper/KetNoi.cs
@@ -8,6 +8,6 @@ namespace QLNS.Helper
 {
     public class KetNoi
     {
-        public static string kn { get; } = "Data Source=LAPTOP-JTJE2T8G\\SQLEXPRESS;Initial Catalog=QLNhanSu;Integrated Security=True;";
+        public static string kn { get; } = "Data Source=DESKTOP-LPHMN4E;Initial Catalog=QLNhanSuDotnet;Integrated Security=True;";
     }
 }
